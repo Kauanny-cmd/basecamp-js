2c43525f476fa9dabefda81d719964458fe752fb
kauanny-cmd <kauannyvieira8@gmail.com>
2022-04-14 18:19:58 -0300

    crio e populo o map

--- colecoes/maps.js
+++ colecoes/maps.js
@@ -2,3 +2,12 @@ function getAdmins(map){
     
 }
 
+let users = new Map();
+
+users.set('Amanda', 'ADMIN')
+users.set('Júlia', 'USER')
+users.set('Naldo', 'USER')
+users.set('João', 'ADMIN')
+users.set('Miguel', 'ADMIN')
+users.set('Larissa', 'USER')
+

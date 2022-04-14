d81bb18882651016062b2586029e8f1a08f2ba2d
kauanny-cmd <kauannyvieira8@gmail.com>
2022-04-14 18:05:21 -0300

    filtrando os tipos de erros

--- debug-error/erros.js
+++ debug-error/erros.js
@@ -1,5 +1,6 @@
 function validaArr(arr, num){
-    if(!arr && !num) throw new ReferenceError("Envie os paramentros")
+    try{
+       if(!arr && !num) throw new ReferenceError("Envie os paramentros")
 
     if(typeof arr !== 'object') throw new TypeError("Array não é do tipo objeto")
 
@@ -8,5 +9,20 @@ function validaArr(arr, num){
     if(arr.length !== num) throw new RangeError('Tamanho inválido') 
 
     return arr;
-    
+    }
+    catch(e){
+        if(e instanceof ReferenceError){
+            console.log(e.message)
+            console.log("Erro do tipo ReferenceError")
+        } else if(e instanceof TypeError){
+            console.log(e.message)
+            console.log("Erro do tipo TypeError")
+        } else if(e instanceof RangeError){
+            console.log(e.message)
+            console.log("Erro do tipo RangeError")
+        }
+    }
+
 }
+
+console.log(validaArr([1,2,3,4], 4))

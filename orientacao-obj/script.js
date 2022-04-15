e5762e1cfee1927261fe8131d2f0398b5f6cba0b
kauanny-cmd <kauannyvieira8@gmail.com>
2022-04-15 11:38:50 -0300

    construtor

---
+++ orientacao-obj/script.js
@@ -0,0 +1,9 @@
+class ContaBancaria{
+    constructor(agencia, num, tipo, saldo){
+        this.agencia = agencia;
+        this.num = num;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+    
+}
